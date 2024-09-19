5fb1c4aeca7cd96d31e59a1df1869d30f387686d
Martin Bradbury <martinbradbury@btinternet.com>
2024-09-19 12:16:35 +0100

    imported permission into views and serializer to check if owner

--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,14 @@ from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfieSerializer
 from rest_framework import generics
+from new_database.permissions import IsOwnerOrReadOnly
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfieSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all().order_by('-created_at')
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfieSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
