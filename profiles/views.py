9f0b00068e2f4d9f7e5440ad72392eeefb40b493
Martin Bradbury <martinbradbury@btinternet.com>
2024-09-19 10:58:04 +0100

    added profile details view and url path

--- profiles/views.py
+++ profiles/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfieSerializer
+from rest_framework import generics
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfieSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    serializer_class = ProfieSerializer
+    queryset = Profile.objects.all().order_by('-created_at')
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfieSerializer
+    queryset = Profile.objects.all()
