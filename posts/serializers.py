b286da5a4b86c6cbe95e62c30a94c43b3d28dff4
Martin Bradbury <martinbradbury@btinternet.com>
2024-09-27 12:27:16 +0100

    added like unlike functionality by creating a get like id method.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
 from .models import Post
-from cloudinary.models import CloudinaryField
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     image = serializers.FileField(max_length=None)
+
+    """
+    Allow users to like and unlike with an ID
+    """
+    like_id = serializers.SerializerMethodField()
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+    
+
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:  # 2MB in bytes
@@ -21,4 +37,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'owner', 'created_at', 'updated_at',
             'title', 'content', 'image', 'is_owner',
+            'like_id',
         ]
