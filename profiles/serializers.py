615386ff30ed5f597d67eb7768f04135617d217d
Martin Bradbury <martinbradbury@btinternet.com>
2024-09-27 12:10:52 +0100

    allows users to follow profiles and identify with id

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follow
 
 class ProfieSerializer(serializers.ModelSerializer):
     owner= serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    """
+    Allow users to follow profiles and identify with an ID
+    """
+    following_id = serializers.SerializerMethodField()
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follow.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            print(following)
+            return following.id if following else None
+        return None
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,4 +29,5 @@ class ProfieSerializer(serializers.ModelSerializer):
         fields = [
                     'owner', 'created_at', 'updated_at',
                     'name', 'content', 'image', 'is_owner',
+                    'following_id',
             ]
