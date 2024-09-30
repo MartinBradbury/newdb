a29152a2297df72bfb48bb310974bd69a92f604c
Martin Bradbury <martinbradbury@btinternet.com>
2024-09-30 12:46:26 +0100

    Added root route, pagination, json renderer and naturaltime

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 
     def get_is_owner(self, obj):
@@ -15,7 +26,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'owner', 'created_at', 'updated_at',
-            'title', 'content', 'is_owner',
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content',
         ]
 
--- new_database/urls.py
+++ new_database/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import root_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +29,6 @@ urlpatterns = [
     path('', include('followers.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.registration.urls')), 
+    path('dj-rest-auth/', include('dj_rest_auth.registration.urls')),
+    path('', root_route), 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
