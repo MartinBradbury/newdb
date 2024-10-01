134dce6bcabaa0ee315e1e7d8a35f9b9df114ec8
Martin Bradbury <martinbradbury@btinternet.com>
2024-10-01 09:10:56 +0100

    debug false

--- new_database/settings.py
+++ new_database/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['new_database.herokuapp.com', 'newdb-8cf17c337cd5.herokuapp.com']
 
